c5c763a8e6492b0c669bd8cc83ba64e3d26b135a
Manjunath <121750296+Manju200417@users.noreply.github.com>
2025-04-06 22:40:50 +0530

    Update script.js
    JavaScript Update

--- script.js
+++ script.js
@@ -3,7 +3,15 @@ function calc(data) {
     if (data === 'C') {
         d.value = '';
     } else if (data === '=') {
-        d.value = eval(d.value);
+        try {
+            if (/[^0-9+\-*/.]/.test(d.value) || /[+\-*/]{2,}/.test(d.value)) {
+                d.value = 'Error';
+            } else {
+                d.value = eval(d.value);
+            }
+        } catch {
+            d.value = 'Error';
+        }
     } else {
         d.value += data;
     }
